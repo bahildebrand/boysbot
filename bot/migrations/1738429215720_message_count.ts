fdea3cf2613dceb6a6135dde6a05bf7bef2b85e3
Blake Hildebrand <baugusthildebrand@gmail.com>
2025-02-01 13:52:58 -0500

    add message_count per day table

---
+++ bot/migrations/1738429215720_message_count.ts
@@ -0,0 +1,31 @@
+import type { Kysely } from "kysely";
+import { sql } from "kysely";
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await sql`
+		CREATE TABLE message_per_day
+		(
+			\`day\` Date,
+			\`snowflake\` String,
+			\`count\` UInt32
+		)
+		ENGINE = SummingMergeTree
+		ORDER BY day
+	`.execute(db);
+
+  await sql`
+		CREATE MATERIALIZED VIEW message_per_day_mv TO message_per_day AS
+		SELECT toStartOfDay(timestamp)::Date as day,
+			count() as count,
+			snowflake as snowflake
+		FROM message
+		GROUP BY
+			day,
+			snowflake
+	`.execute(db);
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable("message_per_day_mv").execute();
+  await db.schema.dropTable("message_per_day").execute();
+}
